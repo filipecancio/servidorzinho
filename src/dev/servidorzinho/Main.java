43b3b23b4677c9efaa08150612c990523c46376d
cancio <filipe.cancio@outlook.com>
2021-04-07 18:07:19 -0300

    Criando servidor

--- src/dev/servidorzinho/Main.java
+++ src/dev/servidorzinho/Main.java
@@ -1,8 +1,21 @@
 package dev.servidorzinho;
 
+import java.io.IOException;
+import java.net.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Servidor servidor = new Servidor(12345);
         System.out.println("Olá mundo!");
     }
+}
+
+class Servidor{
+
+    public Servidor(int port) throws IOException {
+        ServerSocket servidor = new ServerSocket(port);
+        System.out.println("Porta 12345 aberta!");
+    }
+
 }
